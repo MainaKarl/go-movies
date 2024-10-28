87e774da9f19008e8fc96b59fd38dfd6670dd6b3
MainaKarl <mainakarlg@gmail.com>
2024-10-28 12:33:23 +0300

    main file updated: db connection and env file initialized

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"go-movie-crud/pkg/models"
+	"go-movie-crud/pkg/services"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
-	"go-movie-crud/pkg/services"
-	"go-movie-crud/pkg/models"
+	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	err = models.InitConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-func main(){
+func main() {
+
+	//db connection
+	db, err := sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	log.Println(db)
+
+	// Example of fetching and saving a movie from TMDB
+	movie, err := services.FetchMovieFromTMDB(db)
+	if err != nil {
+		log.Fatal("Error fetching movie:", err)
+	}
+
+	log.Printf("Fetched movie: %+v\n", movie)
+
 	// Init Router
 	r := mux.NewRouter()
 
@@ -29,4 +60,3 @@ func main(){
 	fmt.Println("Server started on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
-
