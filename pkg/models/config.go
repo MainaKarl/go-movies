6ba1512dd15efe1cd38d207c85d8490e1957f290
MainaKarl <mainakarlg@gmail.com>
2024-10-28 12:33:53 +0300

    updated model struct

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -1,18 +1,38 @@
 package models
 
-import (
-)
-
 type Movie struct {
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
 type Director struct {
 	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
-} 
+	Lastname  string `json:"lastname"`
+}
 
 var Movies []Movie
+
+type TMDBResponse struct {
+	Results []TMDBMovie `json:"results"`
+}
+type TMDBMovie struct {
+	ID           int     `json:"id"`
+	Title        string  `json:"title"`
+	Overview     string  `json:"overview"`
+	ReleaseDate  string  `json:"release_date"`
+	Language     string  `json:"original_language"`
+	Popularity   float64 `json:"popularity"`
+	VoteAverage  float64 `json:"vote_average"`
+	VoteCount    int     `json:"vote_count"`
+	PosterPath   string  `json:"poster_path"`
+	BackdropPath string  `json:"backdrop_path"`
+}
+
+type Config struct {
+	Apikey  string
+	BaseURL string
+}
+
+var NewConfig Config
