7bbdd6d93059688716ce2d5903a6705b3ebffcf0
MainaKarl <mainakarlg@gmail.com>
2024-10-28 15:09:27 +0300

    creation of a movie updated

--- pkg/services/createmovies.go
+++ pkg/services/createmovies.go
@@ -1,19 +1,42 @@
 package services
 
 import (
+	"database/sql"
 	"encoding/json"
-	"net/http"
-	"math/rand"
-	"strconv"
 	"go-movie-crud/pkg/models"
+	"log"
+	"net/http"
 )
 
-
-func CreateMovie(w http.ResponseWriter, r *http.Request){
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	// Open the database connection
+	db, err := sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	var movie models.Movie
-	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.Itoa(rand.Intn(1000000))
-	models.Movies = append(models.Movies, movie)
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Prepare the SQL insert statement
+	query := `
+		INSERT INTO movies (tmdb_id, title, overview, release_date, language, popularity, vote_average, vote_count, poster_path, backdrop_path)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+
+	// Execute the insert statement
+	_, err = db.Exec(query, movie.TMDBID, movie.Title, movie.Overview, movie.ReleaseDate, movie.Language, movie.Popularity, movie.VoteAverage, movie.VoteCount, movie.PosterPath, movie.BackdropPath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Respond with the created movie
+	w.WriteHeader(http.StatusCreated) // 201 Created
 	json.NewEncoder(w).Encode(movie)
 }
