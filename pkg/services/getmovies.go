960b79dec6df9469d9e6e195b5e6b72dc2c09e4a
MainaKarl <mainakarlg@gmail.com>
2024-10-28 14:42:01 +0300

    update to get all movies and movie

--- pkg/services/getmovies.go
+++ pkg/services/getmovies.go
@@ -1,28 +1,66 @@
 package services
 
 import (
+	"database/sql"
 	"encoding/json"
-	"net/http"
-	"github.com/gorilla/mux"
 	"go-movie-crud/pkg/models"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func GetMovies(w http.ResponseWriter, r *http.Request){
+func GetMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
-	var movie models.Movie
-	json.NewEncoder(w).Encode(movie)
+
+	db, err := sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM movies")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var movies []models.Movie
+	for rows.Next() {
+		var movie models.Movie
+		if err := rows.Scan(&movie.ID, &movie.TMDBID, &movie.Title, &movie.Overview, &movie.ReleaseDate, &movie.Language, &movie.Popularity, &movie.VoteAverage, &movie.VoteCount, &movie.PosterPath, &movie.BackdropPath, &movie.CreatedAt, &movie.UpdatedAt); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		movies = append(movies, movie)
+	}
+
+	json.NewEncoder(w).Encode(movies)
 }
 
-func GetMovie(w http.ResponseWriter, r *http.Request){
+func GetMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
-	for _, item := range models.Movies {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
+	db, err := sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var movie models.Movie
+
+	err = db.QueryRow("SELECT * FROM movies WHERE tmdb_id = ?", params["id"]).Scan(&movie.ID, &movie.TMDBID, &movie.Title, &movie.Overview, &movie.ReleaseDate, &movie.Language, &movie.Popularity, &movie.VoteAverage, &movie.VoteCount, &movie.PosterPath, &movie.BackdropPath, &movie.CreatedAt, &movie.UpdatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// If no movie is found, return an empty movie object
+			json.NewEncoder(w).Encode(models.Movie{})
 			return
 		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	json.NewEncoder(w).Encode(&models.Movie{})
+
+	json.NewEncoder(w).Encode(movie)
 }
