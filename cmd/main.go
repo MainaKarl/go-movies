70b679cb8dc16a6301d42a8a3dc18161e8506560
MainaKarl <mainakarlg@gmail.com>
2024-10-28 15:19:22 +0300

    update to main file

--- cmd/main.go
+++ cmd/main.go
@@ -45,10 +45,6 @@ func main() {
 	// Init Router
 	r := mux.NewRouter()
 
-	// Mock Data
-	models.Movies = append(models.Movies, models.Movie{ID: "1", Isbn: "448743", Title: "Movie One", Director: &models.Director{Firstname: "John", Lastname: "Doe"}})
-	models.Movies = append(models.Movies, models.Movie{ID: "2", Isbn: "847564", Title: "Movie Two", Director: &models.Director{Firstname: "Steve", Lastname: "Smith"}})
-
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/movies", services.GetMovies).Methods("GET")
 	r.HandleFunc("/api/movies/{id}", services.GetMovie).Methods("GET")
